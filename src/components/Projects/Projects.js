9c34eff09a996ef782c3402ce5662da4b0419964
Shahrzad Kheradmand <shahrzad.kheradmand@gmail.com>
2023-02-08 20:40:49 -0800

    project cards

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,6 @@
 import './Projects.scss';
 import Data from '../../assets/data/projects.json'
+import { v4 as uuidv4 } from 'uuid';
 
 const Projects = () => {
   
@@ -7,12 +8,16 @@ const Projects = () => {
     <section className='project'>
         <h2 className='project__title'>P R O J E C T S</h2>
         <div className='project__border'></div>
-        <div className='project__container'>
-           {Data.map(e => 
-                // project card
-                <div>hello</div>
+        <ul className='project__container'>
+           {Data.map(e =>
+                <div key={uuidv4()} className='project__card'>
+                    <li className='project__item' 
+                        style={{backgroundImage: `url(${e.photo_url})`}}
+                    ></li>
+                    <p className='project__name'>{e.title}</p>
+                </div> 
             )}
-        </div>
+        </ul>
     </section>
   )
 }
