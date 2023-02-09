a858867a4235fee4796d0164b94996ffa23ae813
Shahrzad Kheradmand <shahrzad.kheradmand@gmail.com>
2023-02-08 20:02:43 -0800

    project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,17 @@
-import './Projects.scss'
+import './Projects.scss';
+import Data from '../../assets/data/projects.json'
 
 const Projects = () => {
+  
   return (
     <section className='project'>
         <h2 className='project__title'>P R O J E C T S</h2>
         <div className='project__border'></div>
         <div className='project__container'>
-            gallery view
+           {Data.map(e => 
+                // project card
+                <div>hello</div>
+            )}
         </div>
     </section>
   )
